3746330e8c4b716e5b50c05a927425518b66ec12
GT <aymibhatti0@gmail.com>
2026-08-04 15:28:27 +0500

    Add product and price retrieval with total calculation

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function buyNow() {
     alert("Thank you for your order!");
 }
+let product = localStorage.getItem("product");
+let price = localStorage.getItem("price");
+
+document.getElementById("product").value = product;
+document.getElementById("price").value = price;
+
+function calculateTotal(){
+    let quantity = document.getElementById("quantity").value;
+    let total = price * quantity;
+
+    document.getElementById("total").innerHTML = "Total: Rs " + total;
+}
